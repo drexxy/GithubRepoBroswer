2e4b6ac9caaf3b4c37c9e59768560b2dcd4a1a05
Ben Hornsby <ben.hornsby@pharmaseal.co>
2020-04-22 12:10:18 +0100

    Adds User schema

---
+++ lib/github_repo_browser/accounts/user.ex
@@ -0,0 +1,20 @@
+defmodule GithubRepoBrowser.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :provider, :string
+    field :token, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :email, :provider, :token])
+    |> validate_required([:username, :email, :provider, :token])
+  end
+end
