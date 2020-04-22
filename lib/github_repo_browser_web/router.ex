ac92e5ee58ed95b02dd8f4e46c5b5d57b70cb324
Ben Hornsby <ben.hornsby@pharmaseal.co>
2020-04-22 15:18:12 +0100

    Creates plug for setting user to conn
    Allows access to user for future authentication steps

--- lib/github_repo_browser_web/router.ex
+++ lib/github_repo_browser_web/router.ex
@@ -7,6 +7,7 @@ defmodule GithubRepoBrowserWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug GithubRepoBrowserWeb.Plugs.SetUser
   end
 
   pipeline :api do
