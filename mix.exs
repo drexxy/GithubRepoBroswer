fa08b1896c7685d873a6d5b2a76ee0c1aff26fe2
Ben Hornsby <ben.hornsby@pharmaseal.co>
2020-04-22 11:29:10 +0100

    Adds Ueberauth dependency

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule GithubRepoBrowser.MixProject do
   def application do
     [
       mod: {GithubRepoBrowser.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ueberauth, :ueberauth_github]
     ]
   end
 
@@ -42,7 +42,9 @@ defmodule GithubRepoBrowser.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ueberauth, "~> 0.6"},
+      {:ueberauth_github, "~> 0.7"}
     ]
   end
 
